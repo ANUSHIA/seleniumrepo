0cd004a6f4096b03b3a110f7d4070a82867ab1fa
ANUSHIA <anuga10@gmail.com>
2023-08-17 09:00:47 -0700

    TC1

---
+++ src/test/Launch.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.openqa.selenium.chrome.*;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\USER\\Selenium\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+
+	}
+
+}
